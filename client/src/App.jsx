77c379ce20a77009d0e6216d3df61d6b4132a6c2
Eve_rest2 <yaelfavresectioninformatique@gmail.com>
2026-06-02 13:01:39 +0200

    Changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,47 @@
-import { useState } from 'react';
-import FrontPage from './modules/frontpage';
+import { useState, useEffect, useNavigate } from 'react';
+import LoadingPage from './modules/loadingpage.jsx';
+import FrontPage from './modules/frontpage.jsx';
 import './tailwind-setup.css';
 
 function App() {
   const [page, setPage] = useState(1);
 
+  // •  Définir un nombre random entre 5000 et 7000
+  function getRandomInt(min, max) {
+    const minCeiled = Math.ceil(min);
+    const maxFloored = Math.floor(max);
+    return Math.floor(Math.random() * (minCeiled - maxFloored) + maxFloored);
+  }
+
+  // •  Temps de chargement
+  useEffect(() => {
+    setTimeout(() => {
+      setPage(2);
+      console.log("RAPIDE")
+    },
+      // 200
+      getRandomInt(5870, 9500)
+    );
+
+  }, []);
+
+  // •  Gestion des pages
   switch (page) {
     case 1:
+      return (<>
+        <LoadingPage />
+      </>)
+    case 2:
       return (<>
         <FrontPage />
       </>)
 
-    case 2:
+    case 3:
       return (<div className="bg-black text-white p-10">
+        ERROR
       </div>)
   };
+
 }
 
 export default App;
